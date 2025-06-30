9963819526374997fae6123da2d8a883ffb681ce
Tim Brunson <tjbru@amazon.com>
2025-06-30 20:59:49 +0000

    Updated key mappings

--- nvim/after/plugin/keymap/telescope.lua
+++ nvim/after/plugin/keymap/telescope.lua
@@ -2,10 +2,23 @@ local Remap = require("tjbru.keymap")
 local nnoremap = Remap.nnoremap
 
 nnoremap("<C-p>", ":Telescope")
-nnoremap("<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input(\"Grep For > \")})<CR>")
-nnoremap("<C-p>", ":lua require('telescope.builtin').git_files()<CR>")
-nnoremap("<leader>ff", ":lua require('telescope.builtin').find_files()<CR>")
+nnoremap("<leader>gf", ":lua require('telescope.builtin').git_files()<CR>")
+vim.keymap.set('n', '<leader>gm', function()
+    require('telescope.builtin').git_status({
+        git_icons = {
+            added = 'A',
+            changed = 'M',
+            copied = 'C',
+            deleted = 'D',
+            renamed = 'R',
+            unmerged = 'U',
+            untracked = '?',
+        }
+    })
+end, { noremap = true, silent = true })
 
-nnoremap("<leader>pw", ":lua require('telescope.builtin').grep_string{ search = vim.fn.expand(\"<cword>\")}<CR>")
-nnoremap("<leader>pb", ":lua require('telescope.builtin').buffers()<CR>")
-nnoremap("<leader>vh", ":lua require('telescope.builtin').help_tags()<CR>")
+nnoremap("<leader>ff", ":lua require('telescope.builtin').find_files()<CR>")
+nnoremap("<leader>fb", ":lua require('telescope.builtin').buffers()<CR>")
+nnoremap("<leader>fs", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input(\"Grep For > \")})<CR>")
+nnoremap("<leader>fw", ":lua require('telescope.builtin').grep_string{ search = vim.fn.expand(\"<cword>\")}<CR>")
+
