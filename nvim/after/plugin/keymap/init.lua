808602e3d713c43b86d4de92ce2e3d3ed0f4fada
Tim Brunson <tjbru@amazon.com>
2025-07-07 15:42:45 +0000

    Added reload keymap and comments

--- nvim/after/plugin/keymap/init.lua
+++ nvim/after/plugin/keymap/init.lua
@@ -4,22 +4,40 @@ local vnoremap = Remap.vnoremap
 local xnoremap = Remap.xnoremap
 local nmap = Remap.nmap
 
+-- Open Netrw file explorer
 nnoremap("<leader>ee", ":Ex<CR>")
+
+-- Show Undotree
 nnoremap("<leader>u", ":UndotreeShow<CR>")
 
+-- Move selected lines down and maintain visual selection
 vnoremap("J", ":m '>+1<CR>gv=gv")
+-- Move selected lines up and maintain visual selection
 vnoremap("K", ":m '<-2<CR>gv=gv")
 
+-- Yank from cursor to end of line (consistent with D and C)
 nnoremap("Y", "yg$")
+-- Keep cursor centered when navigating through search results
 nnoremap("n", "nzzzv")
 nnoremap("N", "Nzzzv")
+-- Join lines but keep cursor position
 nnoremap("J", "mzJ`z")
 
+-- Paste over selection without copying the deleted text (black hole register)
 xnoremap("<leader>p", "\"_dp")
 
+-- Yank to system clipboard
 nnoremap("<leader>y", "\"+y")
 vnoremap("<leader>y", "\"+y")
 nmap("<leader>Y", "\"+Y")
 
+-- Delete to black hole register (don't copy on delete)
 nnoremap("<leader>d", "\"_d")
 vnoremap("<leader>d", "\"_d")
+
+-- Reload the current buffer with Ctrl+L
+nnoremap("<leader>r", "<cmd>edit<CR>")
+
+-- Reload all buffers with <leader>R
+nnoremap("<leader>R", "<cmd>bufdo e!<CR>")
+
--- nvim/lua/tjbru/keymap.lua
+++ nvim/lua/tjbru/keymap.lua
@@ -1,20 +1,42 @@
+-- Keymap utility module
+-- Provides convenience functions for setting up keymaps in different modes
 local M = {}
 
+-- Helper function to create keymap binding functions
+-- @param op - The mode to bind the keymap to (n=normal, v=visual, i=insert, x=visual block)
+-- @param outer_opts - Default options to apply to all keymaps created with this binding
+-- @return A function that sets keymaps with the specified mode and options
 function bind(op, outer_opts)
     outer_opts = outer_opts or {noremap = true}
     return function(lhs, rhs, opts)
+        -- Merge the default options with any provided options
         opts = vim.tbl_extend("force",
             outer_opts,
             opts or {}
         )
+        -- Set the keymap using Neovim's API
         vim.keymap.set(op, lhs, rhs, opts)
     end
 end
 
+-- Normal mode mapping (allows recursive mapping)
+-- Use for mappings that need to trigger other mappings
 M.nmap = bind("n", {noremap = false})
+
+-- Normal mode non-recursive mapping
+-- Use for most normal mode mappings to prevent unexpected behavior
 M.nnoremap = bind("n")
+
+-- Visual mode non-recursive mapping
+-- For commands that operate on visual selections
 M.vnoremap = bind("v")
+
+-- Visual block mode non-recursive mapping
+-- For commands specific to block selections (Ctrl+v)
 M.xnoremap = bind("x")
+
+-- Insert mode non-recursive mapping
+-- For mappings that trigger while in insert mode
 M.inoremap = bind("i")
 
 return M
