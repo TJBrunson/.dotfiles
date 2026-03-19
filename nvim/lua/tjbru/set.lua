0b6aded6e889756033a8e0aac11dfd458c1f4ee6
Tim Brunson <tjbru@amazon.com>
2026-03-19 16:18:44 +0000

    Make copy to clipboard work

--- nvim/lua/tjbru/set.lua
+++ nvim/lua/tjbru/set.lua
@@ -37,3 +37,15 @@ vim.opt.colorcolumn = "120"
 vim.opt.tags = "./tags;,./.tags;,./src/tags;,.src/.tags;"
 
 vim.g.mapleader = " "
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
